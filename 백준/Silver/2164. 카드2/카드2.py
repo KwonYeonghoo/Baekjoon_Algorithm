5054e50869764346fcd942936495b7d107585ef6
Hoo <kupa10000@naver.com>
2024-05-23 21:26:18 +0900

    [Silver IV] Title: 카드2, Time: 124 ms, Memory: 117056 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,56 @@
+class Queue:
+    def __init__(self, max_size=1000000):
+        self.dat = [0] * max_size
+        self.head = 0
+        self.tail = 0
+
+    def push(self, x):
+        self.dat[self.tail] = x
+        self.tail += 1
+
+    def pop(self):
+        if self.head == self.tail:
+            return -1
+        self.head += 1
+        return self.dat[self.head-1]
+
+    def front(self):
+        if self.head == self.tail:
+            return -1
+        return self.dat[self.head]
+
+    def back(self):
+        if self.head == self.tail:
+            return -1
+        return self.dat[self.tail - 1]
+    
+    def size(self):
+        return self.tail - self.head
+    
+    def empty(self):
+        if self.head == self.tail:
+            return 1
+        return 0
+    
+    def __str__(self):
+        return str(self.dat)
+    
+# 1. pop
+# 2. head -> tail
+# 3. size == 1 이 되면 break
+
+N = int(input())
+
+queue = Queue()
+
+for i in range(1, N+1):
+    queue.push(i)
+
+while queue.size() >= 2:
+    queue.pop()
+    # print(queue.head, queue.tail)
+    queue.dat[queue.tail] = queue.pop()
+    queue.tail += 1
+    # print(queue.head, queue.tail)
+
+print(queue.front())
