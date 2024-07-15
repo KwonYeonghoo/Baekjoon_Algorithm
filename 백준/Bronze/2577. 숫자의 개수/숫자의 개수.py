9a6c1b7a597936f9df78bdc35eef5ff4e2fa2d8f
Hoo <kupa10000@naver.com>
2024-07-15 20:51:05 +0900

    [Bronze II] Title: 숫자의 개수, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.py
@@ -1,16 +1,9 @@
-arr = [0] * 10
-A = int(input())
-B = int(input())
-C = int(input())
+# 숫자의 개수
+A, B, C = [int(input()) for _ in range(3)]
+num = str(A * B * C)
+arr = [0 for _ in range(10)]
+for n in num:
+    arr[int(n)] += 1
 
-result = str(A*B*C)
-
-for n in result:
-    # n은 0~9 사이
-    # arr의 인덱스도 0~9 사이
-    n = int(n)
-    arr[n] += 1
-
-# print(result)
 for i in arr:
-    print(i)
+    print(str(i))
