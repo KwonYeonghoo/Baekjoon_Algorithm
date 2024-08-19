1ce0500f373106087487ea6dc674665288878db8
Hoo <kupa10000@naver.com>
2024-08-19 21:14:19 +0900

    [Silver III] Title: N과 M (6), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.py
@@ -0,0 +1,22 @@
+N, M = tuple(map(int, input().split()))
+arr = sorted(list(map(int, input().split())))
+vis = [0 for _ in range(N)]
+ans = []
+
+def backtracking(curr):
+    if curr == M:
+        print(' '.join(map(str, ans)))
+        return
+    for i in range(N):
+        if vis[i] == 1:
+            continue
+        if len(ans)>0 and ans[-1] >= arr[i]:
+            continue
+        ans.append(arr[i])
+        vis[i] = 1
+        backtracking(curr+1)
+        ans.pop()
+        vis[i] = 0
+        
+backtracking(0)
+        
