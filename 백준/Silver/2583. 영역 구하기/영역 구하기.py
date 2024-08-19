f7311119d0bb3e30ecbcdd992a96b661d2c5c92d
Hoo <kupa10000@naver.com>
2024-08-19 20:56:03 +0900

    [Silver I] Title: 영역 구하기, Time: 68 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,15 +1,14 @@
+
 from collections import deque
 
 M, N, K = tuple(map(int, input().split()))
-rectangles = [list(map(int, input().split())) for _ in range(K)]
-
-arr = [[0 for _ in range(N)] for _ in range(M)]
-vis = [[0 for _ in range(N)] for _ in range(M)]
+areas = [list(map(int, input().split())) for _ in range(K)]
 
 dx = [0, 1, 0, -1]
 dy = [1, 0, -1, 0]
 
-ans = []
+arr = [[0 for _ in range(N)] for _ in range(M)]
+vis = [[0 for _ in range(N)] for _ in range(M)]
 
 def OOB(nx, ny):
     if nx<0 or nx>=M or ny<0 or ny>=N:
@@ -17,23 +16,23 @@ def OOB(nx, ny):
     else:
         return False
 
-# 직사각형 표시하기
-for rectangle in rectangles:
-    x1, y1 = rectangle[0], rectangle[1]
-    x2, y2 = rectangle[2], rectangle[3]
+for area in areas:
+    x1, y1, x2, y2 = area
     for i in range(y1, y2):
         for j in range(x1, x2):
             arr[i][j] = 1
 
-# 모든 영역 순회하기
+count = 0
+sizes = []
 for i in range(M):
     for j in range(N):
         if arr[i][j] == 1 or vis[i][j] == 1:
             continue
         q = deque()
-        q.append([i,j])
         vis[i][j] = 1
-        area_size = 1
+        q.append([i,j])
+        count += 1
+        size = 1
         while len(q):
             x, y = q.popleft()
             for dir in range(4):
@@ -41,10 +40,10 @@ for i in range(M):
                 ny = y + dy[dir]
                 if OOB(nx, ny) or vis[nx][ny] == 1 or arr[nx][ny] == 1:
                     continue
-                q.append([nx,ny])
                 vis[nx][ny] = 1
-                area_size += 1
-        ans.append(area_size)
+                q.append([nx, ny])
+                size += 1
+        sizes.append(size)
 
-print(len(ans))
-print(' '.join(map(str, sorted(ans))))
+print(count)
+print(' '.join(map(str, sorted(sizes))))
