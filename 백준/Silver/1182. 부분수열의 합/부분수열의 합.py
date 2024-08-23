9007a52e539c07c0ea71440ae5017f3de53cc845
Hoo <kupa10000@naver.com>
2024-08-23 17:36:00 +0900

    [Silver II] Title: 부분수열의 합, Time: 212 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -1,21 +1,17 @@
 N, S = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
-vis = [0 for _ in range(N)]
 
 count = 0
-
-def f(k, sum):
-    global count
-    # 탈출 조건
-    if k == N:
+def backtracking(curr, sum):
+    if curr == N:
         if sum == S:
+            global count
             count += 1
         return
-    # k번째 숫자를 포함 한거, 안한거
-    f(k+1, sum)
-    f(k+1, sum + arr[k])
-
-f(0, 0)
+    backtracking(curr+1, sum + arr[curr])
+    backtracking(curr+1, sum)
+    
+backtracking(0,0)
 if S == 0:
     count -= 1
 print(count)
