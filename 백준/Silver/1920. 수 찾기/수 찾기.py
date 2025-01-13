3b792ef2d0899661d8f7cca2f9228ebe7d225d23
Hoo <kupa10000@naver.com>
2025-01-13 14:32:43 +0900

    [Silver IV] Title: 수 찾기, Time: 404 ms, Memory: 49008 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,28 +1,31 @@
-
-
+# O(NM) 불가능 --> O(M log N)으로 풀어야함
+# 이분 탐색
 N = int(input())
-arr1 = sorted(list(map(int, input().split())))
+arr_n = sorted(list(map(int, input().split())))
 M = int(input())
-arr2 = list(map(int, input().split()))
+arr_m = list(map(int, input().split()))
 
-def compare(x, k):
-    if x <= k:
+
+def binary_search(arr, val):
+    low, high = -1, len(arr)
+    while low + 1 < high:
+        mid = (low + high) // 2
+        if arr[mid] >= val:
+            high = mid
+        else:
+            low = mid
+
+    if high == len(arr):
+        return False
+
+    if arr[high] == val:
         return True
     else:
         return False
 
-def binary_search(arr1, k):
-    low, high = -1, len(arr1)
-    while low + 1 < high:
-        mid = (low + high) // 2
-        is_true = compare(arr1[mid], k)
-        if is_true:
-            low = mid
-        else:
-            high = mid
-    if arr1[low] == k:
-        return 1
-    return 0
-    
-for k in arr2:
-    print(binary_search(arr1, k))
+
+for val in arr_m:
+    if binary_search(arr_n, val):
+        print(1)
+    else:
+        print(0)
