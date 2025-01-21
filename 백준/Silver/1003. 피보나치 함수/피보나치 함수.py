50c57237846d6599a54668889ba034e570574aac
Hoo <kupa10000@naver.com>
2025-01-21 14:52:47 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -1,16 +1,15 @@
-dp_table = [-1 for _ in range(41)]
-zero_table = [-1 for _ in range(41)]
-one_table = [-1 for _ in range(41)]
+# dp_table
+zero_table = [0] * (40 + 1)
+one_table = [0] * (40 + 1)  # 각각 0과 1이 출력되는 횟수를 저장할 dp 테이블
 
-dp_table[0], zero_table[0], one_table[0] = 0, 1, 0
-dp_table[1], zero_table[1], one_table[1] = 1, 0, 1
+zero_table[0] += 1
+one_table[1] += 1
 
-for i in range(2, 41):
-    dp_table[i] = dp_table[i-1] + dp_table[i-2]
-    zero_table[i] = zero_table[i-1] + zero_table[i-2]
-    one_table[i] = one_table[i-1] + one_table[i-2]
+for i in range(2, 40 + 1):
+    zero_table[i] = zero_table[i - 2] + zero_table[i - 1]
+    one_table[i] = one_table[i - 2] + one_table[i - 1]
 
 T = int(input())
 for _ in range(T):
-    n = int(input())
-    print(zero_table[n], one_table[n])
+    N = int(input())
+    print(zero_table[N], one_table[N])
