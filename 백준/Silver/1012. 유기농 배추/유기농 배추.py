8f0f13391f936150f689a41007290754b7e1d065
Hoo <kupa10000@naver.com>
2025-01-25 19:29:54 +0900

    [Silver II] Title: 유기농 배추, Time: 232 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,47 +1,47 @@
-# 유기농 배추
-
+import sys
 from collections import deque
 
 
-dx = [1, 0, -1, 0]
+dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+
 def OOB(nx, ny):
-    if nx < 0 or nx >= N or ny < 0 or ny >= M:
+    if nx < 0 or ny < 0 or nx >= N or ny >= M:
         return True
     else:
         return False
-    
-def find_min_earthworm(M,N,K):
-    arr = [[0 for _ in range(M)] for _ in range(N)]
+
+
+T = int(input())
+for _ in range(T):
+    M, N, K = map(int, input().split())
+    arr = [[0] * M for _ in range(N)]
+    visited = [[0] * M for _ in range(N)]
+
     for _ in range(K):
-        a,b = list(map(int, input().split()))
-        arr[b][a] = 1
-    vis = [[0 for _ in range(M)] for _ in range(N)]
-    
+        X, Y = map(int, input().split())
+        arr[Y][X] = 1
+
+    Q = deque()
+
     count = 0
     for i in range(N):
         for j in range(M):
-            if arr[i][j] == 0 or vis[i][j] == 1:
-                vis[i][j] = 1
+            if arr[i][j] == 0 or visited[i][j] == 1:
                 continue
-            q = deque()
-            q.append([i,j])
-            vis[i][j] = 1
-            
-            while len(q):
-                x, y = q.popleft()
+            Q.append([i, j])
+            visited[i][j] = 1
+            count += 1
+            while Q:
+                x, y = Q.popleft()
                 for dir in range(4):
                     nx = x + dx[dir]
                     ny = y + dy[dir]
-                    if OOB(nx, ny) or vis[nx][ny] == 1 or arr[nx][ny] == 0:
+                    if OOB(nx, ny) or visited[nx][ny] == 1:
                         continue
-                    vis[nx][ny] = 1
-                    q.append([nx, ny])
-            count += 1
-    return count
-
-T = int(input())
-for i in range(T):
-    M, N, K = list(map(int, input().split()))
-    print(find_min_earthworm(M,N,K))
+                    if arr[nx][ny] == 0:
+                        continue
+                    Q.append([nx, ny])
+                    visited[nx][ny] = 1
+    print(count)
