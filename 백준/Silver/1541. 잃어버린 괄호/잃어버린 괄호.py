4c0f822745905effac53422ac19148134df855c5
Hoo <kupa10000@naver.com>
2025-01-30 17:43:20 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,19 +1,9 @@
-# 식의 결과가 최소가 돼야함
-# - 뒤에 오는 수가 가능한 한 커야함
-# - 뒤에 +가 오는 경우에만 괄호 치면 됨 --> .split('-')
-
 formula_list = input().split("-")
 result = []
 for f in formula_list:
     num = 0
-    temp = ""
-    for c in f:
-        if ord(c) != 43:
-            temp += c
-        else:
-            num += int(temp)
-            temp = ""
-    num += int(temp)
+    for c in f.split("+"):
+        num += int(c)
     result.append(num)
 
 curr_num = result[0]
