80917e935b7098ba541b01e81859083cd4a68e21
Hoo <kupa10000@naver.com>
2025-02-05 10:56:42 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 460 ms, Memory: 164240 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -5,20 +5,21 @@ import sys
 # 26개 알파벳에 대해 누적합 리스트 생성
 # ord('a'): 97
 S = "_" + sys.stdin.readline().strip()
-q = int(input())
-prefix_sum = [[0] * len(S) for _ in range(26)]
-for i in range(26):
-    target = chr(i + 97)
+q = int(sys.stdin.readline().strip())
+prefix_sum = {s: [0] * len(S) for s in list(set(S))}
+for target in list(prefix_sum.keys()):
     count = 0
     for j in range(len(S)):
         if S[j] == target:
             count += 1
-        prefix_sum[i][j] = count
-# print(prefix_sum)
+        prefix_sum[target][j] = count
 
+# print(prefix_sum)
 for _ in range(q):
-    a, l, r = map(str, input().split())
+    a, l, r = map(str, sys.stdin.readline().strip().split())
     l, r = int(l) + 1, int(r) + 1
-    idx = ord(a) - 97
-    result = prefix_sum[idx][r] - prefix_sum[idx][l - 1]
+    if a not in list(prefix_sum.keys()):
+        print(0)
+        continue
+    result = prefix_sum[a][r] - prefix_sum[a][l - 1]
     print(result)
