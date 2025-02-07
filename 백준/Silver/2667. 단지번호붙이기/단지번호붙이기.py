d437fbd2e264bb31f596ce6f0ed47e7125fcc4b9
Hoo <kupa10000@naver.com>
2025-02-07 10:46:48 +0900

    [Silver I] Title: 단지번호붙이기, Time: 56 ms, Memory: 34984 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,41 +1,40 @@
-
+import sys
 from collections import deque
-
 
 N = int(input())
 arr = [input() for _ in range(N)]
-vis = [[0 for _ in range(N)] for _ in range(N)]
+visited = [[0] * N for _ in range(N)]
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
 
 def OOB(nx, ny):
-    if nx<0 or nx>=N or ny<0 or ny>=N:
+    if nx < 0 or ny < 0 or nx >= N or ny >= N:
         return True
     else:
         return False
-    
-ans = []
 
+
+result = []
 for i in range(N):
     for j in range(N):
-        if arr[i][j] == '0' or vis[i][j] == 1:
+        if arr[i][j] == "0" or visited[i][j] == 1:
             continue
-        q = deque()
-        q.append([i,j])
-        vis[i][j] = 1
-        apt_size = 1
-        while len(q):
-            x, y = q.popleft()
+        count = 1
+        Q = deque([(i, j)])
+        visited[i][j] = 1
+        while Q:
+            x, y = Q.popleft()
             for dir in range(4):
                 nx = x + dx[dir]
                 ny = y + dy[dir]
-                if OOB(nx, ny) or vis[nx][ny] == 1 or arr[nx][ny] == '0':
+                if OOB(nx, ny) or visited[nx][ny] == 1 or arr[nx][ny] == "0":
                     continue
-                q.append([nx,ny])
-                vis[nx][ny] = 1
-                apt_size += 1
-        ans.append(apt_size)
-print(len(ans))
-for a in sorted(ans):
-    print(a)
+                Q.append([nx, ny])
+                visited[nx][ny] = 1
+                count += 1
+        result.append(count)
+
+print(len(result))
+print("\n".join(map(str, sorted(result))))
