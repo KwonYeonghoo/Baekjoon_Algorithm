2a1827f61404e7ab7a40e79778a6fa5034186f36
Hoo <kupa10000@naver.com>
2025-02-07 10:31:15 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 35084 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,39 +1,33 @@
 import sys
 from collections import deque
 
-N, M = list(map(int, input().split()))
+N, M = map(int, input().split())
 arr = [input() for _ in range(N)]
-vis = [[0 for _ in range(M)] for _ in range(N)]
+visited = [[0] * M for _ in range(N)]
 
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
 
 def OOB(nx, ny):
-    if nx < 0 or nx >= N or ny < 0 or ny >= M:
+    if nx < 0 or ny < 0 or nx >= N or ny >= M:
         return True
     else:
         return False
 
 
-ans = 0
-q = deque()
-q.append([0, 0])
-vis[0][0] = 1
-
-while len(q):
-    x, y = q.popleft()
-    if x == (N - 1) and y == (M - 1):
-        print(vis[x][y])
+depth = 1
+visited[0][0] = depth
+Q = deque([(0, 0, depth)])
+while Q:
+    x, y, d = Q.popleft()
+    if x == N - 1 and y == M - 1:
+        print(d)
         break
-    count = 0
     for dir in range(4):
         nx = x + dx[dir]
         ny = y + dy[dir]
-        if OOB(nx, ny) or arr[nx][ny] == "0" or vis[nx][ny] != 0:
+        if OOB(nx, ny) or arr[nx][ny] == '0' or visited[nx][ny] != 0:
             continue
-        count += 1
-        q.append([nx, ny])
-        vis[nx][ny] = vis[x][y] + 1
-    if count != 0:
-        ans += 1
+        Q.append((nx, ny, d + 1))
+        visited[nx][ny] = d + 1
