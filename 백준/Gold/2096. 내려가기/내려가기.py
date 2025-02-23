d5489a3db9d3c462284810c6a105a616f3d0d512
Hoo <kupa10000@naver.com>
2025-02-23 14:07:15 +0900

    [Gold V] Title: 내려가기, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2096. 내려가기/내려가기.py
+++ 백준/Gold/2096. 내려가기/내려가기.py
@@ -1,30 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-# 메모리 4mb
 N = int(input().strip())
-arr = [list(map(int, input().strip().split())) for _ in range(N)]
-prev_dp = [[arr[0][0], arr[0][0]], [arr[0][1], arr[0][1]], [arr[0][2], arr[0][2]]]
-curr_dp = [[0, 0] for _ in range(3)]
-
-for i in range(1, N):
-    for j in range(3):
-        if j == 0:
-            curr_dp[j][0] = max(prev_dp[j][0], prev_dp[j + 1][0]) + arr[i][j]
-            curr_dp[j][1] = min(prev_dp[j][1], prev_dp[j + 1][1]) + arr[i][j]
-        elif j == 1:
-            curr_dp[j][0] = (
-                max(prev_dp[j - 1][0], prev_dp[j][0], prev_dp[j + 1][0]) + arr[i][j]
-            )
-            curr_dp[j][1] = (
-                min(prev_dp[j - 1][1], prev_dp[j][1], prev_dp[j + 1][1]) + arr[i][j]
-            )
-        else:
-            curr_dp[j][0] = max(prev_dp[j - 1][0], prev_dp[j][0]) + arr[i][j]
-            curr_dp[j][1] = min(prev_dp[j - 1][1], prev_dp[j][1]) + arr[i][j]
-    prev_dp, curr_dp = curr_dp, [[0, 0] for _ in range(3)]
-
-print(
-    max(prev_dp[0][0], prev_dp[1][0], prev_dp[2][0]),
-    min(prev_dp[0][1], prev_dp[1][1], prev_dp[2][1]),
-)
+arr = list(map(int, input().strip().split()))
+max_dp = arr
+min_dp = arr
+for _ in range(N-1):
+    arr = list(map(int, input().strip().split()))
+    max_dp = [arr[0]+max(max_dp[0], max_dp[1]), arr[1]+max(max_dp), arr[2]+max(max_dp[1], max_dp[2])]
+    min_dp = [arr[0]+min(min_dp[0], min_dp[1]), arr[1]+min(min_dp), arr[2]+min(min_dp[1], min_dp[2])]
+print(max(max_dp), min(min_dp))
